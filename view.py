b14fe386a628020cfa90e3c9f923b2c9f222a84a
Yuwei-Mao <77122567+Yuwei-Mao@users.noreply.github.com>
2021-06-18 01:14:09 -0700

    add very basic animation

--- view.py
+++ view.py
@@ -39,20 +39,28 @@ def main():
             break
         if up:
             hero.move(0, -speed)
+            hero.begin_anim()
         if up_up:
             hero.move(0, speed)
+            hero.end_anim()
         if down:
             hero.move(0, speed)
+            hero.begin_anim()
         if down_up:
             hero.move(0, -speed)
+            hero.end_anim()
         if left:
             hero.move(-speed, 0)
+            hero.begin_anim()
         if left_up:
             hero.move(speed, 0)
+            hero.end_anim()
         if right:
             hero.move(speed, 0)
+            hero.begin_anim()
         if right_up:
             hero.move(-speed, 0)
+            hero.end_anim()
 
         hero.update()
         pygame.sprite.groupcollide(
@@ -67,7 +75,13 @@ class Hero(pygame.sprite.Sprite):
 
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('image/hero.png').convert()
+        self.animating = False
+        self.sprites = []
+        self.sprites.append(pygame.image.load('image/hero_f1.png'))
+        self.sprites.append(pygame.image.load('image/hero_f2.png'))
+        self.sprites.append(pygame.image.load('image/hero_f3.png'))
+        self.current = 0
+        self.image = self.sprites[self.current]
         self.image.set_colorkey((0, 0, 0))
         self.rect = self.image.get_rect()
         self.movex = 0
@@ -79,9 +93,22 @@ class Hero(pygame.sprite.Sprite):
         self.movex += x
         self.movey += y
 
+    def begin_anim(self):
+        self.animating = True
+
+    def end_anim(self):
+        self.animating = False
+
     def update(self):
         self.rect.x = self.rect.x + self.movex
         self.rect.y = self.rect.y + self.movey
+        if self.animating:
+            self.current += 0.1
+            if self.current >= len(self.sprites):
+                self.current = 0
+
+            self.image = self.sprites[int(self.current)]
+
 
 
 class Enemy(pygame.sprite.Sprite):
